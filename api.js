7c1b67c96eaead9fb96aae86068868f611bdd555
Swetha-ch370 <117908080+Swetha-ch370@users.noreply.github.com>
2022-12-01 18:08:50 +0530

    changes to add, delete, update heroes using api

--- api.js
+++ api.js
@@ -1,72 +1,68 @@
-var express =require('express');
-var cors=require('cors');
-var app=express();
+var express = require('express');
+var cors = require('cors');
+var app = express();
 app.use(cors());
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 const mongo = require('./database')
-//call back function
 
 app.get("/url", (req, res, next) => {
-    res.json(["HTML","CSS","Javascript","Angular","node"]);
-   });
+   res.json(["HTML", "CSS", "Javascript", "Angular", "node"]);
+});
 
 
-   app.listen(3000, () => {
-    console.log("Server running on port 3000");
-   });
+app.listen(3000, () => {
+   console.log("Server running on port 3000");
+});
 
 
-   app.get("/details",  async(req,res) => {
-    // console.log("request",req.length)
-    var data = await getData();
-    // console.log("in get api", data)
-    res.send(data);
-   });
+app.get("/details", async (req, res) => {
+   var data = await getData();
+   res.send(data);
+});
 
 
-   app.post("/details/delete/:id",async(req,res)=>
-{
-   console.log("in delete",req.params)
-   var result=await mongo.deleteHero(req.params.id);
+app.post("/details/delete/:id", async (req, res) => {
+   console.log("in delete", req.params)
+   var result = await mongo.deleteHero(req.params.id);
    console.log("result: ", result)
-   if(result) {
+   if (result) {
       res.status(200).send(true)
    }
-   else
-   {
+   else {
       res.status(400).send(false)
    }
 })
-app.post("/details/add/:name",async(req,res)=>
-{
-   //  console.log("Post request", req.body, req.params);
-    var result=await mongo.addHero(req.params.name)
-    console.log(result)
-    if(result)
-    {
+
+app.post("/details/add/:name", async (req, res) => {
+   
+   var result = await mongo.addHero(req.params.name)
+   console.log(result)
+   if (result) {
       res.status(200).send(true);
-    }
-    else{
+   }
+   else {
       res.status(400).send(false)
-    }
-      
+   }
+
 })
 
 
-//    app.get("/details", (req, res, next) => {
-//     res.json([
-//         { id: 12, name: 'Dr. Nice' },
-//         { id: 13, name: 'Bombasto' },
-//         { id: 14, name: 'Celeritas' },
-//         { id: 15, name: 'Magneta' },
-//         { id: 16, name: 'RubberMan' },
-//         { id: 17, name: 'Dynama' },
-//         { id: 18, name: 'Dr. IQ' },
-//         { id: 19, name: 'Magma' },
-//         { id: 20, name: 'Tornado' }
-//       ]);
-    async function getData()
-    {
-       var data = await mongo.main();
-    //    console.log("data",data)
-       return data;
-    }
+app.post("/details/update/:id", async (req, res) => {
+   console.log(req.body, req.params)
+   var result = await mongo.updateHero(req.body.name, req.body.id)
+
+   console.log(result)
+   if (result) {
+      res.status(200).send(true);
+   }
+   else {
+      res.status(400).send(false)
+   }
+
+})
+async function getData() {
+   var data = await mongo.main();
+   //    console.log("data",data)
+   return data;
+}
--- database.js
+++ database.js
@@ -1,43 +1,49 @@
-const { MongoClient }= require('mongodb');
+const { MongoClient } = require('mongodb');
 var url = "mongodb://127.0.0.1:27017/";
 const client = new MongoClient(url);
 
-  async function main() {
-    await client.connect();
-    // console.log('Connected successfully to server');
-    const db = client.db("Heroes");
-    const collection = db.collection('Heroes');
-    const findResult = await collection.find({}).toArray();
-    return findResult;
-  }
+async function main() {
+  await client.connect();
+  const db = client.db("Heroes");
+  const collection = db.collection('Heroes');
+  const findResult = await collection.find({}).toArray();
+  return findResult;
+}
 
-  async function addHero(heroName)
-  {
-    await client.connect();
-    const db = client.db("Heroes");
-    const collection = db.collection('Heroes');
-    var id=parseInt(Math.random()*100);
-    var result = await collection.insertOne({id:id,name:heroName})
-    console.log("re,res", result)
-    if(result?.insertedId) {
-      return true;
-    }
-    else
-    return false 
+async function addHero(heroName) {
+  await client.connect();
+  const db = client.db("Heroes");
+  const collection = db.collection('Heroes');
+  var id = parseInt(Math.random() * 100);
+  var result = await collection.insertOne({ id: id, name: heroName })
+  console.log("re,res", result)
+  if (result?.insertedId) {
+    return true;
   }
-  async function deleteHero(heroId)
-  {
-    await client.connect();
-    const db = client.db("Heroes");
-    const collection = db.collection('Heroes');
-    let res = await collection.deleteOne({id:parseInt( heroId)});
-    console.log("really deleted: ", res)
-    return res;
-  }
-  module.exports ={
-    main,
-    addHero,
-    deleteHero
-  }
+  else
+    return false
+}
+async function deleteHero(heroId) {
+  await client.connect();
+  const db = client.db("Heroes");
+  const collection = db.collection('Heroes');
+  let res = await collection.deleteOne({ id: parseInt(heroId) });
+  console.log("really deleted: ", res)
+  return res;
+}
 
-  // deleteHero(15)
+async function updateHero(heroName, heroId) {
+  await client.connect();
+  const db = client.db("Heroes");
+  const collection = db.collection('Heroes');
+  let res = await collection.updateOne({ id: parseInt(heroId) }, { $set: { name: heroName } });
+  console.log("really updated: ", res)
+  return res;
+}
+
+module.exports = {
+  main,
+  addHero,
+  deleteHero,
+  updateHero
+}
